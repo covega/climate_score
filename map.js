ec1f9fd926d05888c47e00a44e112198de9ffefe
John <john.morgan@live.com>
2020-07-03 15:01:28 -0700

    Basic filler text

--- map.js
+++ map.js
@@ -9,7 +9,7 @@ var projection = d3.geo.albersUsa()
 var path = d3.geo.path()
     .projection(projection);
 
-var svg = d3.select("body").append("svg")
+var svg = d3.select("body").append("div").append("svg")
     .attr("width", width)
     .attr("height", height).attr("class", "map");
 
