6e7cfbcd614d737393410ef329bda39df5f10f9f
John <john.morgan@live.com>
2020-07-06 23:05:15 -0700

    First pass at adding bootstrap to get this layout closer to mocks. Much styling still needed, as well as proper assets.

--- map.js
+++ map.js
@@ -1,66 +1,6 @@
-var width = 960,
-    height = 600;
-
-var projection = d3.geo.albersUsa()
-    .scale(1280)
-    .translate([width / 2, height / 2]);
-
-var path = d3.geo.path()
-    .projection(projection);
-
-var svg = d3.select("div.map-div").append("svg")
-    .attr("width", width)
-    .attr("height", height).attr("class", "map");
-
-// Can we use this data? Snagged from mbostocks site, looks like it's open source.
-// We should host it on webiste and make an async request instead of bloating the whole site.
-// We need to host this data and run a server instead of the software crime I commit below. Then,
-// make that function a callback with the following signature:
-// 
-// function ready(error, us, congress)
-// 
-// and call the below:
-// 
-// queue()
-//     .defer(d3.json, "http://source-of-data/us.json")
-//     .defer(d3.json, "http://source-of-data/us-congress-113.json")
-//     .await(ready);
 
 
-// vars "us" and "congress" are hoisted from the imports in head of index.html. This is terrible practice.
-
-svg.append("defs").append("path")
-  .attr("id", "land")
-  .datum(topojson.feature(us, us.objects.land))
-  .attr("d", path);
-
-svg.append("clipPath")
-  .attr("id", "clip-land")
-.append("use")
-  .attr("xlink:href", "#land");
-
-svg.append("g")
-  .attr("class", "districts")
-  .attr("clip-path", "url(#clip-land)")
-.selectAll("path")
-  .data(topojson.feature(congress, congress.objects.districts).features)
-.enter().append("path")
-  .attr("d", path).attr("class", "district")
-.append("title")
-  .text(function(d) { return d.id; });
-
-svg.append("path")
-  .attr("class", "district-boundaries")
-  .datum(topojson.mesh(congress, congress.objects.districts, function(a, b) { return a !== b && (a.id / 1000 | 0) === (b.id / 1000 | 0); }))
-  .attr("d", path);
-
-svg.append("path")
-  .attr("class", "state-boundaries")
-  .datum(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; }))
-  .attr("d", path);
-
-d3.select(self.frameElement).style("height", height + "px");
-
+// Toggle an elements visibility.
 function toggleVisibility(element){
   if (element.style.visibility == "visible") {
     element.style.visibility='hidden';
@@ -95,20 +35,107 @@ function onClickDistrict() {
     p_node.appendChild(text_node);
     card.appendChild(p_node);
   }
-  // Now Append the info
-      // <p>Candidate Name</p>
-      // <p>Climbate Cabinet Ranking</p>
-      // <p>Action Needed</p>
-      // <p>District info</p>
-      // <p>Quote</p>
-      //   <p>Quote</p>  
+}
 
+// Add the onclick listeners
+// TODO(jmorg): Use event listeners instead.
+function addOnClickToDistricts() {
+  // Get all districts with class ".district" and register this onclick
+  var districts = document.getElementsByClassName("district");
+  for (var i = 0; i < districts.length; ++i) {
+    district = districts[i];
+    district.onclick = function() {onClickDistrict()};
+  }
+}
+
+function drawMap() {
+  console.log(window.innerWidth);
+  console.log(window.innerLength);
+  var width = document.getElementById('map-div').clientWidth;
+  var height = document.getElementById('map-div').clientHeight;
+
+  var projection = d3.geo.albersUsa()
+      .scale(1000)
+      .translate([width / 2, height / 2]);
+
+  var path = d3.geo.path()
+      .projection(projection);
+
+  var svg = d3.select("div.map-div").append("svg")
+      .attr("width", width)
+      .attr("height", height).attr("class", "map").attr("id", "map");
+
+  // Can we use this data? Snagged from mbostocks site, looks like it's open source.
+  // We should host it on webiste and make an async request instead of bloating the whole site.
+  // We need to host this data and run a server instead of the software crime I commit below. Then,
+  // make that function a callback with the following signature:
+  // 
+  // function ready(error, us, congress)
+  // 
+  // and call the below:
+  // 
+  // queue()
+  //     .defer(d3.json, "http://source-of-data/us.json")
+  //     .defer(d3.json, "http://source-of-data/us-congress-113.json")
+  //     .await(ready);
+
+
+  // vars "us" and "congress" are hoisted from the imports in head of index.html. This is terrible practice.
+
+  svg.append("g").append("path")
+    .attr("id", "land")
+    .datum(topojson.feature(us, us.objects.land))
+    .attr("d", path).attr("fill", "#bbb");
+
+  svg.append("clipPath")
+    .attr("id", "clip-land")
+  .append("use")
+    .attr("xlink:href", "#land");
+
+  svg.append("path")
+    .attr("class", "state-boundaries")
+    .datum(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; }))
+    .attr("d", path);
+
+  d3.select(self.frameElement).style("height", height + "px");
+}
+
+// Unused for now
+function drawDistricts() {
+  svg.append("g")
+    .attr("class", "districts")
+    .attr("clip-path", "url(#clip-land)")
+  .selectAll("path")
+    .data(topojson.feature(congress, congress.objects.districts).features)
+  .enter().append("path")
+    .attr("d", path).attr("class", "district")
+  .append("title")
+    .text(function(d) { return d.id; });
+
+  svg.append("path")
+    .attr("class", "district-boundaries")
+    .datum(topojson.mesh(congress, congress.objects.districts, function(a, b) { return a !== b && (a.id / 1000 | 0) === (b.id / 1000 | 0); }))
+    .attr("d", path);
 
 }
 
-// Get all districts with class ".district" and register this onclick
-var districts = document.getElementsByClassName("district");
-for (var i = 0; i < districts.length; ++i) {
-  district = districts[i];
-  district.onclick = function() {onClickDistrict()};
+function onResizeRedrawMap() {
+  console.log("resize triggered");
+  var map = document.getElementById("map");
+  while (map.firstChild) {
+    map.removeChild(map.firstChild);
+  }
+  map.remove();
+  drawMap();
+  addOnClickToDistricts();
 }
+
+function main() {
+  drawMap();
+  addOnClickToDistricts();
+  window.addEventListener("resize", onResizeRedrawMap)
+}
+
+main();
+
+
